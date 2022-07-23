1af9844b00f4203f56f37520f437f985554b8ea8
Viktor Kopan <victorkopan98@gmail.com>
2022-07-23 13:58:01 +0600

    feat: created global ValidationPipe and validation exception

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,13 @@
 import {ApiProperty} from '@nestjs/swagger';
+import {IsEmail, IsString, Length} from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({example: 'user@mail.ru', description: 'Email'})
+  @IsString({message: 'Дожно быть строкой'})
+  @IsEmail({}, {message: 'Неккоректный Email'})
   readonly email: string;
   @ApiProperty({example: '123456', description: 'Пароль пользователя'})
+  @IsString({message: 'Дожно быть строкой'})
+  @Length(4, 16, {message: 'Не меньше 4 и не больше 16'})
   readonly password: string;
 }

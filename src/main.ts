e3075f4f98cc706073863420557712cf81415b93
Viktor Kopan <victorkopan98@gmail.com>
2022-08-12 19:09:41 +0600

    fix: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import {ValidationPipe} from './pipes/validation.pipe';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Movies Project')
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -6,6 +6,7 @@ export class CreateUserDto {
   @IsString({message: 'Дожно быть строкой'})
   @IsEmail({}, {message: 'Неккоректный Email'})
   readonly email: string;
+
   @ApiProperty({example: '123456', description: 'Пароль пользователя'})
   @IsString({message: 'Дожно быть строкой'})
   @Length(4, 16, {message: 'Не меньше 4 и не больше 16'})

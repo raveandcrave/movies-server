0d4d091b6394dae9540f21698c7573765d9c2ead
Viktor Kopan <victorkopan98@gmail.com>
2023-08-28 21:14:02 +0600

    cors origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CORS_ORIGIN || true,
     credentials: true,
   });
 
